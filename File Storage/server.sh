44adf437f12c4fa3ebd825f564feaa6bdc3dcef1
Simone Schiavone <76122911+SimoneSchiavone@users.noreply.github.com>
2021-12-31 09:25:10 +0100

    Delete server.sh

--- File Storage/server.sh
+++
@@ -1,5 +0,0 @@
-clear
-chmod +x server.sh
-#!/bin/bash
-gcc -Wall -Werror -pedantic-errors -g server_utils/serverutils.c server_utils/data_structures.c server.c -o server -lpthread
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./server
