376ff794917fdba75172f4dc7fac1bf21fd1dd37
Simone Schiavone <76122911+SimoneSchiavone@users.noreply.github.com>
2022-01-28 16:56:11 +0100

    fix valgrind flag

--- File Storage/test/test1/test1.sh
+++ File Storage/test/test1/test1.sh
@@ -5,8 +5,8 @@ echo $server_pid
 
 sleep 2
 
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./client -p -t 200 -w ./test/test1/File_di_prova -D Espulsi -r ./test/test1/File_di_prova/minnie.txt -u ./test/test1/File_di_prova/minnie.txt 
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./client -p -t 200 -l ./test/test1/File_di_prova/minnie.txt -W ./test/test1/File_di_prova/minnie.txt -d Letti -R 0 -c ./test/test1/File_di_prova/minnie.txt
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./client -p -t 200 -w ./test/test1/File_di_prova -D Espulsi -r ./test/test1/File_di_prova/minnie.txt -u ./test/test1/File_di_prova/minnie.txt 
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./client -p -t 200 -l ./test/test1/File_di_prova/minnie.txt -W ./test/test1/File_di_prova/minnie.txt -d Letti -R 0 -c ./test/test1/File_di_prova/minnie.txt
 
 sleep 2
 echo "Client terminato invio segnale al server"
