be01397a89aa06d743b549352bb17d178673696a
Simone Schiavone <76122911+SimoneSchiavone@users.noreply.github.com>
2022-01-28 16:57:19 +0100

    fix valgrind flag

--- File Storage/test/test2/test2.sh
+++ File Storage/test/test2/test2.sh
@@ -1,12 +1,12 @@
 echo "Avvio server"
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./server &
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./server &
 server_pid=$!
 echo $server_pid
 
 sleep 2
 
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./client -t 4500 -w ./test/test2/files -D Espulsi 
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./client -D Espulsi -W test/test2/files/so.txt,test/test2/files/so.txt -d Letti -R 0
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./client -t 4500 -w ./test/test2/files -D Espulsi 
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./client -D Espulsi -W test/test2/files/so.txt,test/test2/files/so.txt -d Letti -R 0
 
 sleep 2
 echo "Client terminato invio segnale al server"
